4398daaa630301439e2a69262485c51a3db51644
Alex Gustafsson <alex-gustafsson@live.se>
2024-05-26 19:37:53 +0200

    Clean up package

--- Package.swift
+++ Package.swift
@@ -7,7 +7,6 @@ let package = Package(
   name: "RSSBar",
   platforms: [.macOS(.v14)],
   products: [
-    // Products define the executables and libraries a package produces, making them visible to other packages.
     .executable(
       name: "RSSBar",
       targets: ["RSSBar"]),
@@ -23,8 +22,6 @@ let package = Package(
       url: "https://github.com/pointfreeco/swift-custom-dump", from: "1.0.0"),
   ],
   targets: [
-    // Targets are the basic building blocks of a package, defining a module or a test suite.
-    // Targets can depend on other targets in this package and products from dependencies.
     .executableTarget(
       name: "RSSBar",
       resources: [
