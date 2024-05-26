a2874f52ac0b257fa6acf5acde7df8cddf7cbd01
Alex Gustafsson <alex-gustafsson@live.se>
2024-05-26 16:14:37 +0200

    Bundle icon with module

--- Package.swift
+++ Package.swift
@@ -26,7 +26,10 @@ let package = Package(
     // Targets are the basic building blocks of a package, defining a module or a test suite.
     // Targets can depend on other targets in this package and products from dependencies.
     .executableTarget(
-      name: "RSSBar"
+      name: "RSSBar",
+      resources: [
+        .copy("Resources/icon.png")
+      ]
     ),
     .target(
       name: "RSSKit",
