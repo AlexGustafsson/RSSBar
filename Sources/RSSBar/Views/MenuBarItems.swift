8396c3d4ef06748dcde89000d642c8184c4a31cc
Alex Gustafsson <alex-gustafsson@live.se>
2024-06-02 08:57:47 +0200

    Refactor menu bar views

---
+++ Sources/RSSBar/Views/MenuBarItems.swift
@@ -0,0 +1,131 @@
+import Foundation
+import SwiftUI
+
+private struct MenuBarItemButtonStyle: ButtonStyle {
+  var isHovering: Bool = false
+  @State private var isPressed = false
+
+  func makeBody(configuration: Self.Configuration) -> some View {
+    configuration.label
+      .padding(EdgeInsets(top: 4, leading: 6, bottom: 4, trailing: 6))
+      .foregroundColor(.white)
+      .background(
+        self.isHovering ? .primary.opacity(0.1) : Color.clear
+      )
+      .cornerRadius(6)
+  }
+}
+
+struct MenuBarFeedItem: View {
+  var title: any StringProtocol
+  var url: URL
+
+  @State private var isHovering = false
+  @State private var showFeedItems = false
+
+  var body: some View {
+    Button(action: {
+      showFeedItems = true
+    }) {
+      HStack(alignment: .center) {
+        Favicon(url: url).frame(width: 24, height: 24)
+        Text(title).frame(maxWidth: .infinity, alignment: .leading)
+          .padding(
+            EdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+          ).foregroundColor(.primary)
+
+        CountBadge(
+          value: .constant(12)
+        )
+      }
+    }
+    .buttonStyle(
+      MenuBarItemButtonStyle(isHovering: isHovering)
+    ).onHover(perform: { flag in
+      self.isHovering = flag
+    }).popover(isPresented: $showFeedItems, arrowEdge: .trailing) {
+      VStack(alignment: .leading, spacing: 0) {
+        MenuBarTextItem(
+          action: {
+            showFeedItems = false
+            NSWorkspace.shared.open(URL(string: "https://example.com")!)
+          },
+          title: "Feed title",
+          subtitle: "12h ago",
+          systemName: "rectangle.portrait.and.arrow.right"
+        )
+        MenuBarTextItem(
+          action: {
+            showFeedItems = false
+            NSWorkspace.shared.open(URL(string: "https://example.com")!)
+          },
+          title: "Feed title",
+          subtitle: "12h ago",
+          systemName: "rectangle.portrait.and.arrow.right"
+        )
+
+        MenuBarTextItem(
+          action: {
+            showFeedItems = false
+            NSWorkspace.shared.open(URL(string: "https://example.com")!)
+          },
+          title: "Feed title",
+          subtitle: "12h ago",
+          systemName: "rectangle.portrait.and.arrow.right"
+        )
+        Spacer()
+        MenuBarTextItem(
+          action: {
+            // TODO
+            showFeedItems = false
+          },
+          title: "Mark all as read"
+        )
+      }.frame(width: 250, height: 250).padding(
+        EdgeInsets(top: 6, leading: 6, bottom: 6, trailing: 6)
+      )
+    }
+  }
+}
+
+struct MenuBarTextItem: View {
+  var action: () -> Void
+  var title: any StringProtocol
+  var subtitle: (any StringProtocol)?
+  var systemName: String?
+
+  var body: some View {
+    MenuBarItem {
+      action()
+    } label: {
+      HStack {
+        VStack(alignment: .leading) {
+          Text(title).foregroundColor(.primary)
+          if subtitle != nil {
+            Text(subtitle!).font(.footnote).foregroundColor(.primary)
+          }
+        }.frame(maxWidth: .infinity, alignment: .topLeading)
+        if systemName != nil {
+          Image(systemName: systemName!)
+            .foregroundColor(.primary)
+        }
+      }
+    }
+  }
+}
+
+struct MenuBarItem<Label>: View where Label: View {
+  var action: () -> Void
+  var label: () -> Label
+  @State private var isHovering = false
+
+  var body: some View {
+    Button(action: action, label: label)
+      .buttonStyle(
+        MenuBarItemButtonStyle(isHovering: isHovering)
+      ).onHover(
+        perform: { flag in
+          self.isHovering = flag
+        })
+  }
+}
--- Sources/RSSBar/Views/MenuBarView.swift
+++ Sources/RSSBar/Views/MenuBarView.swift
@@ -1,132 +1,6 @@
 import Foundation
 import SwiftUI
 
-struct MyButtonStyle: ButtonStyle {
-  var isHovering: Bool
-  @State private var isPressed = false
-
-  func makeBody(configuration: Self.Configuration) -> some View {
-    configuration.label
-      .padding(EdgeInsets(top: 4, leading: 6, bottom: 4, trailing: 6))
-      .foregroundColor(.white)
-      .background(
-        self.isHovering ? .primary.opacity(0.1) : Color.clear
-      )
-      .cornerRadius(6)
-  }
-
-}
-
-struct ListItem: View {
-  let url: URL
-
-  @State private var isHovering = false
-  @State private var presentContent = false
-
-  var body: some View {
-    Button(action: {
-      presentContent = true
-    }) {
-      HStack(alignment: .center) {
-        Favicon(url: url).frame(width: 24, height: 24)
-        Text("GitHub.com").frame(maxWidth: .infinity, alignment: .leading)
-          .padding(
-            EdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
-          ).foregroundColor(.primary)
-
-        CountBadge(
-          value: .constant(12)
-        )
-      }
-    }
-    .buttonStyle(
-      MyButtonStyle(isHovering: isHovering)
-    ).onHover(perform: { flag in
-      self.isHovering = flag
-    }).popover(isPresented: $presentContent, arrowEdge: .trailing) {
-      VStack(alignment: .leading, spacing: 0) {
-        ListItemLite2(
-          title: "Feed title", subtitle: "12h ago",
-          action: {
-            presentContent = false
-            NSWorkspace.shared.open(URL(string: "https://example.com")!)
-          })
-        ListItemLite2(
-          title: "Feed title", subtitle: "12h ago",
-          action: {
-            presentContent = false
-            NSWorkspace.shared.open(URL(string: "https://example.com")!)
-
-          })
-        ListItemLite2(
-          title: "Feed title", subtitle: "12h ago",
-          action: {
-            presentContent = false
-            NSWorkspace.shared.open(URL(string: "https://example.com")!)
-          })
-        Spacer()
-        ListItemLite(
-          title: "Mark all as read",
-          action: {
-            // TODO
-            presentContent = false
-          })
-      }.frame(width: 250, height: 250).padding(
-        EdgeInsets(top: 6, leading: 6, bottom: 6, trailing: 6)
-      )
-    }
-  }
-}
-
-struct ListItemLite: View {
-  var title: any StringProtocol
-  var action: () -> Void
-  @State private var isHovering = false
-
-  var body: some View {
-    Button(action: action) {
-      Text(title).frame(maxWidth: .infinity, alignment: .leading)
-        .padding(
-          EdgeInsets(top: 1, leading: 2, bottom: 1, trailing: 2)
-        ).foregroundColor(.primary)
-    }
-    .buttonStyle(
-      MyButtonStyle(isHovering: isHovering)
-    ).onHover(
-      perform: { flag in
-        self.isHovering = flag
-      })
-  }
-}
-
-struct ListItemLite2: View {
-  var title: any StringProtocol
-  var subtitle: any StringProtocol
-  var action: () -> Void
-  @State private var isHovering = false
-
-  var body: some View {
-    Button(action: action) {
-      HStack {
-        VStack(alignment: .leading) {
-          Text(title).foregroundColor(.primary)
-
-          Text(subtitle).font(.footnote).foregroundColor(.primary)
-        }.frame(maxWidth: .infinity, alignment: .topLeading)
-        Image(systemName: "rectangle.portrait.and.arrow.right")
-          .foregroundColor(.primary)
-
-      }
-    }
-    .buttonStyle(
-      MyButtonStyle(isHovering: isHovering)
-    ).onHover(
-      perform: { flag in
-        self.isHovering = flag
-      })
-  }
-}
-
 struct MenuBarView: View {
   @Environment(\.openWindow) private var openWindow
   @Environment(\.closeMenuBar) private var closeMenuBar
@@ -137,18 +11,22 @@ struct MenuBarView: View {
 
   var body: some View {
     VStack(alignment: .leading) {
-      ListItemLite(
-        title: "Fetch now",
+      MenuBarTextItem(
         action: {
           // TODO
           closeMenuBar()
-        })
+        },
+        title: "Fetch now"
+      )
 
       Divider()
 
       LazyVStack(alignment: .leading, spacing: 0) {
-        ListItem(url: URL(string: "https://github.com")!)
-        ListItem(url: URL(string: "https://news.ycombinator.com")!)
+        MenuBarFeedItem(
+          title: "GitHub", url: URL(string: "https://github.com")!)
+        MenuBarFeedItem(
+          title: "Hacker News",
+          url: URL(string: "https://news.ycombinator.com")!)
       }
 
       Divider()
@@ -159,8 +37,12 @@ struct MenuBarView: View {
         EdgeInsets(top: 2, leading: 6, bottom: 0, trailing: 0)
       ).foregroundStyle(.secondary)
       LazyVStack(alignment: .leading, spacing: 0) {
-        ListItem(url: URL(string: "https://github.com")!)
-        ListItem(url: URL(string: "https://news.ycombinator.com")!)
+        MenuBarFeedItem(
+          title: "GitHub", url: URL(string: "https://github.com")!)
+        MenuBarFeedItem(
+          title: "Hacker News",
+          url: URL(string: "https://news.ycombinator.com")!)
+
       }
 
       Divider()
@@ -173,35 +55,34 @@ struct MenuBarView: View {
         .secondary
       )
       LazyVStack(alignment: .leading, spacing: 0) {
-        ListItem(url: URL(string: "https://github.com")!)
-        ListItem(url: URL(string: "https://news.ycombinator.com")!)
+        MenuBarFeedItem(
+          title: "GitHub", url: URL(string: "https://github.com")!)
+        MenuBarFeedItem(
+          title: "Hacker News",
+          url: URL(string: "https://news.ycombinator.com")!)
+
       }
 
       Divider()
 
       VStack(alignment: .leading, spacing: 0) {
-        ListItemLite(
-          title: "About",
+        MenuBarTextItem(
           action: {
             openWindow(id: "about")
             closeMenuBar()
-          })
 
-        ListItemLite(
-          title: "Settings...",
+          }, title: "About")
+        MenuBarTextItem(
           action: {
             try? openSettings()
             closeMenuBar()
-          }
-        )
 
-        ListItemLite(
-          title: "Quit",
+          }, title: "Settings...")
+        MenuBarTextItem(
           action: {
-            closeMenuBar()
             quitApp()
-          })
-
+            closeMenuBar()
+          }, title: "Quit")
       }
     }.padding(
       EdgeInsets(top: 6, leading: 6, bottom: 6, trailing: 6)
