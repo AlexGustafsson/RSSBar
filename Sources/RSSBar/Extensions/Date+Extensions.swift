3a3709a81098be61cf2df2a5402ed0df15a4f316
Alex Gustafsson <alex-gustafsson@live.se>
2024-06-07 16:25:03 +0200

    Remove decimals from formatted date distance

--- Sources/RSSBar/Extensions/Date+Extensions.swift
+++ Sources/RSSBar/Extensions/Date+Extensions.swift
@@ -4,32 +4,35 @@ extension Date {
   func formattedDistance(to other: Date) -> String {
     var diff = self.distance(to: other)
 
-    let prefix = diff > 0 ? "in " : ""
-    let postfix = diff <= 0 ? " ago" : ""
+    let prefix = diff < 0 ? "in " : ""
+    let postfix = diff >= 0 ? " ago" : ""
 
     diff = floor(abs(diff))
 
     let seconds = diff
+    if seconds == 0 {
+      return "just now"
+    }
     if seconds < 60 {
-      return "\(prefix)\(seconds)s\(postfix)"
+      return "\(prefix)\(Int(seconds))s\(postfix)"
     }
 
     let minutes = floor(seconds / 60)
     if minutes < 60 {
-      return "\(prefix)\(minutes)m\(postfix)"
+      return "\(prefix)\(Int(minutes))m\(postfix)"
     }
 
     let hours = floor(minutes / 60)
     if hours < 60 {
-      return "\(prefix)\(hours)h\(postfix)"
+      return "\(prefix)\(Int(hours))h\(postfix)"
     }
 
     let days = floor(hours / 24)
     if days < 24 {
-      return "\(prefix)\(days)d\(postfix)"
+      return "\(prefix)\(Int(days))d\(postfix)"
     }
 
     let years = floor(days / 365)
-    return "\(prefix)\(years)y\(postfix)"
+    return "\(prefix)\(Int(years))y\(postfix)"
   }
 }
