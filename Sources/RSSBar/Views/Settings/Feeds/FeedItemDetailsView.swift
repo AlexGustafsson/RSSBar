bb57a04e061a19c742cd207f50de29aca7263ca3
Alex Gustafsson <alex-gustafsson@live.se>
2024-06-19 12:18:41 +0200

    Fix multiline text in feed detail items

--- Sources/RSSBar/Views/Settings/Feeds/FeedItemDetailsView.swift
+++ Sources/RSSBar/Views/Settings/Feeds/FeedItemDetailsView.swift
@@ -102,7 +102,9 @@ struct FeedItemDetailsView: View {
                 Favicon(url: item.url)
                   .frame(width: 24, height: 24)
                 VStack(alignment: .leading) {
-                  Text(item.title).foregroundColor(.primary)
+                  Text(item.title).foregroundColor(.primary).lineLimit(1)
+                    .truncationMode(.tail)
+
                   Text(item.date?.formattedDistance(to: Date()) ?? "")
                     .foregroundColor(.primary).font(.footnote)
                 }
