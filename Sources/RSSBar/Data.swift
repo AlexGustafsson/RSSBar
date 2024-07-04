691c4ddec039c3436048bc5798a3fb7b03a98389
Alex Gustafsson <alex-gustafsson@live.se>
2024-07-04 19:51:09 +0200

    Add import/export functionality

--- Sources/RSSBar/Data.swift
+++ Sources/RSSBar/Data.swift
@@ -27,3 +27,68 @@ func initializeModelContainer() throws -> ModelContainer {
     for: FeedGroup.self, Feed.self, FeedItem.self,
     configurations: configuration)
 }
+
+func exportModelData(to directory: URL, modelContext: ModelContext) throws {
+  let groups = try modelContext.fetch(FetchDescriptor<FeedGroup>())
+
+  let encoder = JSONEncoder()
+  encoder.outputFormatting = .prettyPrinted
+  let data = try encoder.encode(groups)
+
+  let dateFormatter = DateFormatter()
+  dateFormatter.dateFormat = "yyy-MM-dd-HH-mm-ss"
+
+  try data.write(
+    to: directory.appending(
+      component:
+        "rssbar-export-\(dateFormatter.string(for: Date.now)!).json")
+  )
+}
+
+func importModelData(from path: URL, modelContext: ModelContext) throws {
+  let decoder = JSONDecoder()
+
+  let groups = try decoder.decode(
+    [FeedGroup].self, from: Data(contentsOf: path))
+
+  // TODO: Due to SwiftData and relations, we can't really modify groups.feeds
+  // without crashing (without an error). It seems like there's some meddling
+  // where a context is necessary. If we just store the data immediately, then
+  // we don't have an issue. But if we try to perform some modifications before
+  // then we crash...
+  // TODO: How do we best clean up group order? One way would be to try to
+  // validate the orders and if they're bad, just replace with the index of the
+  // group instead
+  // for group in groups {
+  //   // Clean up item order
+  //   for (index, item) in group.feeds.enumerated() { item.order = index }
+  // }
+
+  try modelContext.transaction {
+    // NOTE: Should coalesce delete on FeedGroup and Feed, but let's
+    // make sure all potential dangling items are deleted as well
+    try modelContext.delete(model: FeedGroup.self)
+    try modelContext.delete(model: Feed.self)
+    try modelContext.delete(model: FeedItem.self)
+
+    for group in groups {
+      modelContext.insert(group)
+    }
+
+    // Save is implicit
+  }
+}
+
+func resetModelData(modelContext: ModelContext) throws {
+  try modelContext.transaction {
+    // NOTE: Should coalesce delete on FeedGroup and Feed, but let's
+    // make sure all potential dangling items are deleted as well
+    try modelContext.delete(model: FeedGroup.self)
+    try modelContext.delete(model: Feed.self)
+    try modelContext.delete(model: FeedItem.self)
+
+    // Save is implicit
+  }
+
+  try DiskCache.shared.removeAll()
+}
--- Sources/RSSBar/Views/Settings/Advanced/AdvancedSettingsView.swift
+++ Sources/RSSBar/Views/Settings/Advanced/AdvancedSettingsView.swift
@@ -2,6 +2,7 @@ import Combine
 import Foundation
 import SwiftData
 import SwiftUI
+import UniformTypeIdentifiers
 import os
 
 private let logger = Logger(
@@ -9,6 +10,7 @@ private let logger = Logger(
 
 struct AdvancedSettingsView: View {
   @State private var presentResetDialog = false
+  @State private var presentImportDialog = false
   @AppStorage("enableFaviconsFetching") private var enableFaviconsFetching =
     true
 
@@ -31,6 +33,48 @@ struct AdvancedSettingsView: View {
         }
       }
 
+      Section("Data") {
+        Button("Export...") {
+          let panel = NSOpenPanel()
+          panel.allowsMultipleSelection = false
+          panel.canChooseDirectories = true
+          panel.canChooseFiles = false
+          panel.prompt = "Export"
+          if panel.runModal() == .OK && panel.url != nil {
+            try? exportModelData(to: panel.url!, modelContext: modelContext)
+          }
+        }
+        Button("Import...") { presentImportDialog = true }
+          .confirmationDialog(
+            "Imported data will replace any existing data. Continue?",
+            isPresented: $presentImportDialog
+          ) {
+            Button("Continue", role: .destructive) {
+              let panel = NSOpenPanel()
+              panel.allowsMultipleSelection = false
+              panel.canChooseDirectories = false
+              panel.allowedContentTypes = [UTType.json]
+              panel.prompt = "Import"
+              if panel.runModal() == .OK && panel.url != nil {
+                do {
+                  try importModelData(
+                    from: panel.url!, modelContext: modelContext)
+                } catch {
+                  logger.error(
+                    "Failed to import data: \(error, privacy: .public)")
+                }
+                updateIcon?()
+              }
+            }
+            .keyboardShortcut(.delete)
+          } message: {
+            Text(
+              "All existing groups, feeds and history will be removed."
+            )
+          }
+          .dialogIcon(Image(systemName: "square.and.arrow.down.fill"))
+      }
+
       Section("Actions") {
         Button("Reset...") { presentResetDialog = true }
           .confirmationDialog(
@@ -39,14 +83,13 @@ struct AdvancedSettingsView: View {
           ) {
             Button("Reset", role: .destructive) {
               do {
-                // NOTE: Should coalesce delete on FeedGroup and Feed, but let's
-                // make sure all potential dangling items are deleted as well
-                try modelContext.delete(model: FeedGroup.self)
-                try modelContext.delete(model: Feed.self)
-                try modelContext.delete(model: FeedItem.self)
-                try DiskCache.shared.removeAll()  // TODO: Reset settings to default
-                updateIcon?()
-              } catch { logger.error("Failed to clear data") }
+                try resetModelData(modelContext: modelContext)
+              } catch {
+                logger.error(
+                  "Failed to reset data: \(error, privacy: .public)")
+              }
+              // TODO: Reset settings to default
+              updateIcon?()
             }
             .keyboardShortcut(.delete)
           } message: {
@@ -55,7 +98,6 @@ struct AdvancedSettingsView: View {
             )
           }
           .dialogIcon(Image(systemName: "arrow.clockwise.circle.fill"))
-
       }
     }
     .formStyle(.grouped)
