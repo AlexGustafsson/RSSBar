a516acc89da12e9387f9ddd35398e56630496c1b
Alex Gustafsson <alex-gustafsson@live.se>
2024-07-18 20:48:51 +0200

    Improve date distance formatting

--- Sources/RSSBar/Extensions/Date+Extensions.swift
+++ Sources/RSSBar/Extensions/Date+Extensions.swift
@@ -11,18 +11,33 @@ extension Date {
 
     let seconds = diff
     if seconds == 0 { return "just now" }
-    if seconds < 60 { return "\(prefix)\(Int(seconds))s\(postfix)" }
+    if seconds < 60 {
+      return
+        "\(prefix)\(Int(seconds)) second\(seconds > 1 ? "s" : "")\(postfix)"
+    }
 
     let minutes = floor(seconds / 60)
-    if minutes < 60 { return "\(prefix)\(Int(minutes))m\(postfix)" }
+    if minutes < 60 {
+      return
+        "\(prefix)\(Int(minutes)) minute\(minutes > 1 ? "s" : "")\(postfix)"
+    }
 
     let hours = floor(minutes / 60)
-    if hours < 60 { return "\(prefix)\(Int(hours))h\(postfix)" }
+    if hours < 60 {
+      return "\(prefix)\(Int(hours)) hours\(hours > 1 ? "s" : "")\(postfix)"
+    }
 
     let days = floor(hours / 24)
-    if days < 365 { return "\(prefix)\(Int(days))d\(postfix)" }
+    if days < 30 {
+      return "\(prefix)\(Int(days)) day\(days > 1 ? "s" : "")\(postfix)"
+    }
+
+    let months = floor(days / 30)
+    if days < 365 {
+      return "\(prefix)\(Int(months)) month\(months > 1 ? "s" : "")\(postfix)"
+    }
 
     let years = floor(days / 365)
-    return "\(prefix)\(Int(years))y\(postfix)"
+    return "\(prefix)\(Int(years)) year\(years > 1 ? "s" : "")\(postfix)"
   }
 }
